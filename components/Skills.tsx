882e53800aa42025c623f804333c7d890b669f4d
koustav <koustavm.aeie2018@nsec.ac.in>
2023-04-29 13:30:16 +0530

    skill

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -14,12 +14,12 @@ function Skills({ }: Props) {
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
       
-      className='h-screen flex relative text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 items-center mt-5'>
+      className='h-screen flex relative text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 items-center'>
       <h3 className='absolute top-[7rem] md:top-20 lg:top-20 uppercase tracking-[20px] text-gray-500 text-2xl'>
         Skills
       </h3>
       <h3 className='absolute top-36 uppercaes tracking-[3px] text-gray-400'>Hover over for current proficiency status</h3>
-      <div className='grid grid-cols-4 gap-5'>
+      <div className='grid grid-cols-4 gap-5 md:mt-[]'>
         <Skill />
 
       </div>
