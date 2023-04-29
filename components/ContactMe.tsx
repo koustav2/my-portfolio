feae2a357a4c7d1c99626709fc4f4d55a9075262
koustav <koustavm.aeie2018@nsec.ac.in>
2023-04-29 16:54:15 +0530

    commit contact

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -18,21 +18,21 @@ function ContactMe({ }: Props) {
                 <h4 className='text-4xl font-semibold text-center mb-5 underline decoration-[#240b12]'>
                     Let's talk
                 </h4>
-                <div className="space-y-5 md:space-y-10">
+                <div className="space-y-5 md:space-y-5 mb-5">
                     <div className="flex items-center space-x-5 justify-center">
                         <PhoneIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
-                        <p className="text-2xl">+917586894901</p>
+                        <p className="text-xl">+917586894901</p>
                     </div>
                     <div className="flex items-center space-x-5 justify-center">
                         <EnvelopeIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
-                        <p className="text-2xl">maitykoustav2809@gmail.com</p>
+                        <p className="text-xl">maitykoustav2809@gmail.com</p>
                     </div>
                     <div className="flex items-center space-x-5 justify-center">
                         <MapPinIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
-                        <p className="text-2xl">Kolkata</p>
+                        <p className="text-xl">Kolkata</p>
                     </div>
                 </div>
-                <form method='POST' action="https://getform.io/f/d9db5188-8b53-4cc2-882a-ab136bfb3051" className='flex flex-col space-y-2 w-fit mx-auto my-5'>
+                <form method='POST' action="https://getform.io/f/d9db5188-8b53-4cc2-882a-ab136bfb3051" className='flex flex-col space-y-2 w-fit mx-auto my-5 '>
                     <div className='flex space-x-2'>
                         <input type="text" placeholder='Name' className="contactInput" required />
                         <input type="email" placeholder='Email' className="contactInput" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required />
@@ -40,7 +40,7 @@ function ContactMe({ }: Props) {
                     <input type="text" placeholder='Subject' className="contactInput" required />
                     <input type='tel' placeholder='Phone Number' className="contactInput" required />
                     <textarea placeholder='Message' className="contactInput" required />
-                    <button type="submit" className='bg-[#f7ab0a] py-5 rounded-md text-black font-bold text-lg'>
+                    <button type="submit" className='bg-[#f7ab0a] py-5 rounded-md text-black font-bold text-lg hover:bg-blue-900 '>
                         Submit
                     </button>
                 </form>
