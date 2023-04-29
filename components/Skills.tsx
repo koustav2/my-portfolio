fc583f3bc0ce7bf12f631d9a3df539b0eeb309df
koustav <koustavm.aeie2018@nsec.ac.in>
2023-04-29 14:37:16 +0530

    commit

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -14,7 +14,7 @@ function Skills({ }: Props) {
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
       
-      className='h-screen flex relative text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 items-center'>
+      className='h-screen flex relative text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center  items-center'>
       <h3 className='absolute top-[7rem] md:top-20 lg:top-20 uppercase tracking-[20px] text-gray-500 text-2xl'>
         Skills
       </h3>
