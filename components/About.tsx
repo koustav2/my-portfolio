0b1969e67cc5eeff56bd0b1c5fc83887d6bc63bb
koustav <koustavm.aeie2018@nsec.ac.in>
2023-05-29 12:29:48 +0530

    padding

--- components/About.tsx
+++ components/About.tsx
@@ -18,7 +18,7 @@ export default function About({ }: Props) {
                 About
             </h3>
             <motion.img
-                className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[400px] xl:h-[400px] mt-[15rem] md:mt-0 lg:mt-0'
+                className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[400px] xl:h-[400px] mt-[9rem] md:mt-0 lg:mt-0'
                 initial={{
                     x: -200,
                     opacity: 0
@@ -41,7 +41,7 @@ export default function About({ }: Props) {
             <div className="space-y-10  md:px-10 mt-[5rem] md:mt-0 lg:mt-0 md:text-xs">
                 <h4 className="text-sm md:text-xl lg:text-4xl text-gray-400 font-bold">Here is a{" "}
                     <span className="underline decoration-[#000080]">little</span>{" "} background</h4>
-                <p className='text-gray-500 text-sm md:text-xl lg:text-2xl'>
+                <p className='text-gray-500 text-sm md:text-base lg:text-2xl'>
                     I'm koustav. here's a little bit about me... .I am a passionate Front-End Developer, driven by a deep love for
                     crafting seamless user experiences and bringing designs to life.I have coding for 1.5
                     years of now.. I have honed my skills in transforming design concepts into interactive websites, ensuring they are responsive, intuitive, and visually appealing across various devices and platforms
