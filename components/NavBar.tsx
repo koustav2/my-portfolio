13e756b18f2a58290e6b529026472f9cce425fb3
koustav <koustavm.aeie2018@nsec.ac.in>
2023-08-16 19:22:34 +0530

    add

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -48,7 +48,7 @@ function NavBar({ }: Props) {
             <h1 className='uppercase text-sm link link-underline link-underline-black hover:text-pink-500'>Resume</h1>
           </Link>
         </div>
-        <a href="mailto:maitykoustav2911@gmail.com" className='text-gray-400 mt-4'>
+        <a href="mailto:maitykoustav2809@gmail.com" className='text-gray-400 mt-4'>
           <h1 className='uppercase text-sm link link-underline link-underline-black hover:text-pink-500'>Email</h1>
         </a>
       </motion.div>
